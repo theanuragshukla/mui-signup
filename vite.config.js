bd008f780ec438d4a769cd772bfa0323ecebd3fd
theanuragshukla <tachyonanurag@gmail.com>
2022-12-29 15:33:31 +0530

    modified sw and pwa manifest

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,40 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	base: "/mui-signup/",
+	plugins: [
+		react(),
+		VitePWA({
+			registerType: 'autoUpdate', 
+			devOptions: {
+				enabled: true
+			}, 
+			injectRegister: 'auto', 
+			includeAssets: ["favicon.ico", "apple-touch-icon.png"],
+			workbox: {
+				globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+			}, 
+			manifest: {
+				name: "mui-signup",
+				short_name: "signup",
+				description: "lorem",
+				theme_color: "#000000",
+				icons: [
+					{
+						src: "android-chrome-192x192.png",
+						sizes: "192x192",
+						type: "image/png",
+					},
+					{
+						src: "android-chrome-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+					},
+				],
+			},
+		}),
+	],
+});
