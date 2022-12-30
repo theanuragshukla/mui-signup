8a16312c6ae994b27caf3aa08ddf7897e3da5df6
theanuragshukla <tachyonanurag@gmail.com>
2022-12-30 12:34:18 +0530

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/mui-signup/sw.js', { scope: '/mui-signup/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
