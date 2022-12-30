1d68f8fc8bb64ad617c23e38ebcbff4437bb8c7e
theanuragshukla <tachyonanurag@gmail.com>
2022-12-30 18:55:18 +0530

    modified faulty url

--- src/secrets.js
+++ src/secrets.js
@@ -1,4 +1,4 @@
 // const serverUrl = "http://localhost:3000/subscribe"
-const serverUrl = "https://pwa-push-server/subscribe";
+const serverUrl = "https://pwa-push-server.onrender.com/subscribe";
 
 export { serverUrl };
